f696afe2bc526aad172eeb043ae8f95e121d0acb
Biplab Prasad <97167296+bp-cognisaas@users.noreply.github.com>
2022-02-26 15:26:24 +0530

    added url path

---
+++ EmployeeApp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from EmployeeApp import views
+
+urlpatterns=[
+    path(r'^department/$',views.departmentApi),
+    path(r'^department/([0-9]+)$',views.departmentApi)
+]
